c8f6edca4381c2491677a50b12cfbb15fa0967b8
Andrei Popovici <me@andreipopovici.co.uk>
2025-03-16 22:06:22 +0000

    Added auth controller & service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: 'my-secret-key', //process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { RegisterDto, LoginDto } from './dto/auth.dto';
+
+type User = {
+  id: number;
+  username: string;
+  email: string;
+  password: string;
+};
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  // TODO: Replace with a database
+  private users: User[] = [];
+
+  constructor(private readonly jwtService: JwtService) {}
+
+  async register(registerDto: RegisterDto) {
+    const hashedPassword = await bcrypt.hash(registerDto.password, 10);
+
+    const user = {
+      id: this.users.length + 1,
+      ...registerDto,
+      password: hashedPassword,
+    };
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  async login(loginDto: LoginDto) {
+    const user = this.users.find((user) => user.email === loginDto.email);
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      loginDto.password,
+      user.password,
+    );
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    return this.generateToken(user);
+  }
+
+  private generateToken(user: User) {
+    const payload = { sub: user.id, email: user.email };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
